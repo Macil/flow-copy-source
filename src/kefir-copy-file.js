f4af2dae435070144f12c00c0b68ea447ff60842
Chris Cowan <agentme49@gmail.com>
2017-06-24 21:05:40 -0700

    Update fs-extra dependency.

--- src/kefir-copy-file.js
+++ src/kefir-copy-file.js
@@ -4,14 +4,6 @@ var Kefir = require('kefir');
 var fs = require('fs-extra');
 
 module.exports = function kefirCopyFile(src, dest) {
-  return Kefir.stream(emitter => {
-    fs.copy(src, dest, (err) => {
-      if (err) {
-        emitter.error(err);
-      } else {
-        emitter.emit({src, dest});
-      }
-      emitter.end();
-    });
-  });
+  return Kefir.fromPromise(fs.copy(src, dest))
+    .map(() => ({src, dest}));
 };
